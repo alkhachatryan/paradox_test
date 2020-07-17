da9443bc69f6ae3c7a24b4ab2204df56ea0bbbc4
alexey <alexey.khachatryan@gmail.com>
2020-07-17 19:44:33 +0400

    Refactoring: Added PHPDocs to the controllers, small code refactoring

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -7,11 +7,26 @@ use App\Http\Requests\Todos\DeleteTodoRequest;
 use App\Http\Requests\Todos\EditTodoRequest;
 use App\Http\Requests\Todos\GetTodoRequest;
 use App\Todo;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * The resource controller class to work with requests related with Todos model
+ *
+ * Implemented resource actions: list (with filters), create, edit, delete
+*/
 class TodosController extends Controller
 {
+    /**
+     * List the todos ordered by done column.
+     * If "filter" parameter passed and it equals "finished" - get only finished todos,
+     * Otherwise not finished todos will be returned (in case of filled "filter" parameter)
+     *
+     * @param GetTodoRequest $request
+     *
+     * @return JsonResponse
+    */
     public function index(GetTodoRequest $request){
         $todos = Todo::orderBy('done', 'DESC')
             ->with('user');
@@ -19,9 +34,17 @@ class TodosController extends Controller
         if($request->filled('filter'))
             $todos->whereDone($request->input('filter') === 'finished');
 
-        return $todos->get();
+        return response()->json($todos->get(), 200);
     }
 
+    /**
+     * Create a Todo model.
+     * If the user is logged in (Bearer token present and it's right) - save the user ID
+     *
+     * @param CreateTodoRequest $request
+     *
+     * @return JsonResponse
+    */
     public function create(CreateTodoRequest $request){
         $todo = new Todo();
 
@@ -37,11 +60,25 @@ class TodosController extends Controller
         return response()->json($todo, 201);
     }
 
+    /**
+     * Edit the Todo model
+     *
+     * If user filled the title or the status - update it.
+     * If the updating data is empty - return 400 response.
+     *
+     * @param EditTodoRequest $request
+     * @param integer $id
+     *
+     * @return JsonResponse
+    */
     public function edit(EditTodoRequest $request, $id){
         $todo = Todo::find((int)$id);
 
         if(! $todo)
             return response()->json('Not found', 404);
+
+        if(! $request->anyFilled(['title', 'done']))
+            return response()->json('Nothing to update', 400);
 
         if($request->filled('title'))
             $todo->title = $request->input('title');
@@ -54,6 +91,16 @@ class TodosController extends Controller
         return response()->json($todo, 200);
     }
 
+    /**
+     * Delete the Todo model
+     *
+     * If the todo has done = false - inform user, as thenot finished todo cannot be deleted.
+     *
+     * @param DeleteTodoRequest $request
+     * @param integer $id
+     *
+     * @return JsonResponse
+    */
     public function delete(DeleteTodoRequest $request, $id){
         $todo = Todo::find((int)$id);
 
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,16 +7,36 @@ use App\Http\Requests\Users\UserLoginRequest;
 use App\Http\Requests\Users\UserLogoutRequest;
 use App\Http\Requests\Users\UserRegisterRequest;
 use App\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
+/**
+ * The controller class to work with requests related with User model
+ *
+ * Implemented resource actions: index, register, login, logout
+ */
 class UserController extends Controller
 {
+    /**
+     * Get logged in user data.
+     *
+     * @param UserIndexRequest $request
+     *
+     * @return JsonResponse
+    */
     public function index(UserIndexRequest $request){
-        return response($request->user(), 200);
+        return response()->json($request->user(), 200);
     }
 
+    /**
+     * Register a new user.
+     *
+     * @param UserRegisterRequest $request
+     *
+     * @return JsonResponse
+    */
     public function register(UserRegisterRequest $request){
         $user = User::create([
             'name' => $request->input('name'),
@@ -33,30 +53,46 @@ class UserController extends Controller
         ], 201);
     }
 
+    /**
+     * User login.
+     *
+     * Create and return the access token in case of right login and password.
+     *
+     * @param UserRegisterRequest $request
+     * @return JsonResponse
+    */
     public function login(UserLoginRequest $request){
         $user = User::whereEmail($request->input('email'))
             ->first();
 
         if ($user) {
             if(! Hash::check($request->input('password'), $user->password))
-                return response('Wrong password', 422);
+                return response()->json('Wrong password', 422);
 
             $token = $user->createToken('Laravel Password Grant Client')->accessToken;
 
-            return response(['token' => $token], 200);
+            return response()->json(['token' => $token], 200);
         } else {
             $response = 'User does not exist';
 
-            return response($response, 422);
+            return response()->json($response, 422);
         }
     }
 
+    /**
+     * Logout user.
+     * Revoke the access token.
+     *
+     * @param UserRegisterRequest $request
+     *
+     * @return JsonResponse
+    */
     public function logout(UserLogoutRequest $request){
         $token = $request->user()->token();
 
         $token->revoke();
 
         $response = 'You have been successfully logged out!';
-        return response($response, 200);
+        return response()->json($response, 200);
     }
 }
